a9e01f7cad32a0bb9c8581e1920aed907066c8f0
Kutsarit V <kutsarit@web.de>
2020-01-31 22:57:39 +0100

    add super ElephantBrain!

---
+++ Assets/Scripts/ElephantBrain.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ElephantBrain : MonoBehaviour
+{
+	public GameObject waypointsRoot;
+	public float waitTimer = 10;
+
+	// Start is called before the first frame update
+	IEnumerator Start()
+	{
+
+		var agent = GetComponent<NavMeshAgent>();
+
+		var path = new NavMeshPath();
+
+		while (true)
+		{
+			var count = waypointsRoot.transform.childCount;
+			var child = waypointsRoot.transform.GetChild(Random.Range(0, count));
+
+			agent.CalculatePath(child.position, path);
+
+			agent.SetPath(path);
+
+			yield return new WaitForSeconds(waitTimer);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
