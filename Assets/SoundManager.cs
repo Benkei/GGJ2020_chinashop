29dfcfdae241231135647933b3962403abcb0725
ChristianKlotz <www.christianklotz@hotmail.de>
2020-01-31 22:39:03 +0100

    Sound Main Menu

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+}
