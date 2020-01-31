99eafd4b20ba8c8f5db028cd4d3eaf3776c14cc7
Marvin <44087233+git-maspa@users.noreply.github.com>
2020-01-31 23:21:01 +0100

    started plate destruction

---
+++ Assets/Scripts/PlateDestruction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateDestruction : MonoBehaviour
+{
+    public Collider Col;
+    public Rigidbody R;
+    public bool IsDestroyed = false;
+    
+
+    void OnCollisionEnter(Collision col)
+    {
+        IsDestroyed = true;
+        R.isKinematic = false;
+        Col.enabled = false;
+        foreach (var rigidbody in transform.GetComponentsInChildren<Rigidbody>())
+        {
+            rigidbody.isKinematic = true;
+        }
+    }
+
+    void O
+    //foreach(var rigidbody in transform.GetComponentsInChildren<Rigidbody>())
+    //    {
+    //        rigidbody.transform.localPosition = Vector3.zero;
+    //        rigidbody.transform.localRotation = Quaternion.identity;
+    //    }
+}
