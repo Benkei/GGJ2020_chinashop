dba3424452bb83b8ddc4d63977467300af6db034
Kutsarit V <kutsarit@web.de>
2020-01-31 22:08:45 +0100

    update MainMenu add exit button test game level

---
+++ Assets/Scripts/exitGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class exitGame : MonoBehaviour, IPointerClickHandler
+{
+	public void OnPointerClick(PointerEventData eventData)
+	{
+
+		Debug.Log("Exit Game");
+
+		Application.Quit();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
