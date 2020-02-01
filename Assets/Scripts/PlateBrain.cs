4d5c00158b13216eaca2b76912dc4b8e05bc99a1
Kutsarit V <kutsarit@web.de>
2020-02-01 12:16:37 +0100

    add new plate prefab

--- Assets/Scripts/PlateBrain.cs
+++ Assets/Scripts/PlateBrain.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class PlateBrain : MonoBehaviour
 {
+	public GameObject BaseModel;
+	public GameObject BrokenModel;
+
+	void Awake()
+	{
+		BaseModel.SetActive(true);
+		BrokenModel.SetActive(false);
+	}
 
 	void OnCollisionEnter(Collision collision)
 	{
+		BaseModel.SetActive(false);
 
+		if (!BrokenModel.activeSelf)
+		{
+			BrokenModel.SetActive(true);
+			var posi = collision.GetContact(0).point;
+
+			//Debug.Log("explode " + posi);
+
+			foreach (var item in BrokenModel.GetComponentsInChildren<Rigidbody>())
+			{
+				item.AddExplosionForce(100, posi, 100f);
+			}
+		}
 	}
 
 
