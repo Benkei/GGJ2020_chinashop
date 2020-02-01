63ad69ba92c57ac62ff803a218440bfddb0eda14
Kutsarit V <kutsarit@web.de>
2020-02-01 21:00:48 +0100

    update Elephant stuff

--- Assets/Scripts/ElephantBrain.cs
+++ Assets/Scripts/ElephantBrain.cs
@@ -8,11 +8,18 @@ public class ElephantBrain : MonoBehaviour
 	public GameObject waypointsRoot;
 	public float waitTimer = 10;
 
+	Animator animator;
+	NavMeshAgent agent;
+
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+		agent = GetComponent<NavMeshAgent>();
+	}
+
 	// Start is called before the first frame update
 	IEnumerator Start()
 	{
-		var agent = GetComponent<NavMeshAgent>();
-
 		while (true)
 		{
 			var count = waypointsRoot.transform.childCount;
@@ -20,7 +27,27 @@ public class ElephantBrain : MonoBehaviour
 
 			agent.destination = child.position;
 
+			while (agent.velocity.magnitude > 0.1f)
+			{
+				yield return null;
+			}
 			yield return new WaitForSeconds(waitTimer);
+		}
+	}
+
+	void Update()
+	{
+		var speed = agent.velocity.magnitude;
+		//var blend = speed / 1.25f;
+		if (speed > 0.1f)
+		{
+			//animator.speed = blend;
+			animator.SetBool("Walk", true);
+		}
+		else
+		{
+			//animator.speed = 1;
+			animator.SetBool("Walk", false);
 		}
 	}
 
