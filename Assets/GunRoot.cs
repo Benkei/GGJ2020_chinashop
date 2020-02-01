d10085982ba9464f6f967884db853ba98152fbb5
Kutsarit V <kutsarit@web.de>
2020-02-01 23:47:15 +0100

    update gun snapping version

---
+++ Assets/GunRoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRoot : MonoBehaviour
+{
+	public Camera camera;
+
+	public Vector3 anchor = new Vector3(1, 0, 1);
+
+
+	void Start()
+	{
+		var pos = camera.ViewportToWorldPoint(anchor);
+		var localPos = transform.InverseTransformPoint(pos);
+
+		transform.localPosition = localPos;
+	}
+}
