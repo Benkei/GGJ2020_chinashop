7aafa21debff57df11c4799eba734eaca2aefb84
Kutsarit V <kutsarit@web.de>
2020-02-01 17:32:59 +0100

    add dummy game ui

---
+++ Assets/Scripts/UIGameTimerBrain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameTimerBrain : MonoBehaviour
+{
+	public Text textTarget;
+
+	float oldTime = 0;
+	TimeSpan oldSpan;
+
+	public void Update()
+	{
+		if (GameplayManager.currentTime != oldTime)
+		{
+			oldTime = GameplayManager.currentTime;
+			var span = System.TimeSpan.FromSeconds(GameplayManager.maxTime - GameplayManager.currentTime);
+			span = new System.TimeSpan(span.Days, span.Hours, span.Minutes, span.Seconds);
+
+			if (oldSpan != span)
+			{
+				oldSpan = span;
+				textTarget.text = $"{oldSpan.ToString(@"mm\:ss")}";
+			}
+		}
+	}
+}
