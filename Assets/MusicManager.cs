ac1b20633e68832f1d43134b2cdfcf3981cd2331
Kevin Balz <kevin.balz@web.de>
2020-02-02 16:25:12 +0100

    persistent music

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    static bool single = false;
+    void Start()
+    {
+        if (single)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        single = true;
+        DontDestroyOnLoad(gameObject);
+        GetComponent<AudioSource>().Play();
+    }
+}
