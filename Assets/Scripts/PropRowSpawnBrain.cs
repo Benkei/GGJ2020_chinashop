3fecb3f81247bc87850e5e87f511397f64b891d6
Kutsarit V <kutsarit@web.de>
2020-02-02 16:06:57 +0100

    fix shelf

--- Assets/Scripts/PropRowSpawnBrain.cs
+++ Assets/Scripts/PropRowSpawnBrain.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PropRowSpawnBrain : MonoBehaviour
 {
@@ -14,20 +15,50 @@ public class PropRowSpawnBrain : MonoBehaviour
 		var prefabs = Resources.LoadAll<GameObject>(ResourcesFolder);
 
 		Vector3 begin = Vector3.zero;
-		for (int i = 0; i < SpawnCount; i++)
+		int i = 0;
+		for (i = 0; i < SpawnCount; i++)
+		{
+			var prefab = prefabs[Random.Range(0, prefabs.Length)];
+
+			var brain = prefab.GetComponentInChildren<PlateBrain>();
+
+			if (!prefab.name.StartsWith("empty"))
+			{
+				Spawn(begin, prefab);
+			}
+
+			begin += SpawnOffset;
+
+			if (brain != null)
+			{
+				i++;
+				break;
+			}
+		}
+
+		// filter
+		prefabs = prefabs.Where((a) => a.GetComponentInChildren<PlateBrain>() == null).ToArray();
+
+		// fill none interactive props
+		for (; i < SpawnCount; i++)
 		{
 			var prefab = prefabs[Random.Range(0, prefabs.Length)];
 
 			if (prefab.name != "empty")
 			{
-				var go = Object.Instantiate(prefab);
-
-				go.transform.parent = transform;
-				go.transform.localPosition = begin;
-				go.transform.localRotation = Quaternion.identity;
+				Spawn(begin, prefab);
 			}
 
 			begin += SpawnOffset;
 		}
+	}
+
+	private void Spawn(Vector3 begin, GameObject prefab)
+	{
+		var go = Object.Instantiate(prefab);
+
+		go.transform.parent = transform;
+		go.transform.localPosition = begin;
+		go.transform.localRotation = Quaternion.identity;
 	}
 }
