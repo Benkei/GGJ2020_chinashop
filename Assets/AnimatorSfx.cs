e69a337b74e85c770ef805dbd34a226e5d0ddaf2
Kutsarit V <kutsarit@web.de>
2020-02-02 16:34:15 +0100

    add Elephant food fx

--- Assets/AnimatorSfx.cs
+++ Assets/AnimatorSfx.cs
@@ -14,12 +14,22 @@ public class AnimatorSfx : MonoBehaviour
 	AudioSource audioToeBL;
 	AudioSource audioToeBR;
 
+	ParticleSystem fxToeFL;
+	ParticleSystem fxToeFR;
+	ParticleSystem fxToeBL;
+	ParticleSystem fxToeBR;
+
 	private void Awake()
 	{
 		audioToeFL = ToeFL.GetComponent<AudioSource>();
 		audioToeFR = ToeFR.GetComponent<AudioSource>();
 		audioToeBL = ToeBL.GetComponent<AudioSource>();
 		audioToeBR = ToeBR.GetComponent<AudioSource>();
+
+		fxToeFL = ToeFL.GetComponentInChildren<ParticleSystem>();
+		fxToeFR = ToeFR.GetComponentInChildren<ParticleSystem>();
+		fxToeBL = ToeBL.GetComponentInChildren<ParticleSystem>();
+		fxToeBR = ToeBR.GetComponentInChildren<ParticleSystem>();
 	}
 
 	public void SfxStep(string s)
@@ -28,15 +38,19 @@ public class AnimatorSfx : MonoBehaviour
 		{
 			case "fr":
 				audioToeFL.Play();
+				fxToeFL.Play();
 				break;
 			case "fl":
 				audioToeFR.Play();
+				fxToeFR.Play();
 				break;
 			case "bl":
 				audioToeBL.Play();
+				fxToeBL.Play();
 				break;
 			case "br":
 				audioToeBR.Play();
+				fxToeBR.Play();
 				break;
 		}
 	}
