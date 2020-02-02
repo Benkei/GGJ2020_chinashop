b901cddfc5b547595f5099711f8bc8b515c7904b
Kutsarit V <kutsarit@web.de>
2020-02-02 12:28:40 +0100

    add new shelf

---
+++ Assets/Scripts/PropRowSpawnBrain.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropRowSpawnBrain : MonoBehaviour
+{
+	public Vector3 SpawnOffset = Vector3.forward;
+	public int SpawnCount = 2;
+	public string ResourcesFolder = "Prefabs";
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		var prefabs = Resources.LoadAll<GameObject>(ResourcesFolder);
+
+		Vector3 begin = Vector3.zero;
+		for (int i = 0; i < SpawnCount; i++)
+		{
+			var prefab = prefabs[Random.Range(0, prefabs.Length)];
+
+			if (prefab.name != "empty")
+			{
+				var go = Object.Instantiate(prefab);
+
+				go.transform.parent = transform;
+				go.transform.localPosition = begin;
+			}
+
+			begin += SpawnOffset;
+		}
+	}
+}
