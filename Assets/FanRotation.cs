7a05cf6a5f9f360759fecb3778c4ad4e373c28ef
ChristianKlotz <www.christianklotz@hotmail.de>
2020-02-02 10:07:21 +0100

    stuff stuff and the soup

---
+++ Assets/FanRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRot : MonoBehaviour
+{  
+    public GameObject Fan;
+    // Start is called before the first frame update
+    private void Update()
+    {
+        
+    }
+
+    public void Fanrotate (int rotate)
+    {
+        Fan.transform.localRotation *= Quaternion.AngleAxis(-rotate * Time.deltaTime, Vector3.up);
+    }
+}
---
+++ Assets/Fanrotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fanrotate : MonoBehaviour
+{
+    public GameObject Fan;
+    // Start is called before the first frame update
+    private void Update()
+    {
+
+    }
+
+    public void fanrotate(int rotate)
+    {
+        Fan.transform.localRotation *= Quaternion.AngleAxis(-rotate * Time.deltaTime, Vector3.up);
+    }
+}
